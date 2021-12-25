b8276f0b621571afb9930887deeacd1b58f25f37
Todd Meng <tftmeng@gmail.com>
2021-12-24 19:33:28 -0800

    Initial Commit

---
+++ Vozi/VoziApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoziApp.swift
+//  Vozi
+//
+//  Created by Todd Meng on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct VoziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
